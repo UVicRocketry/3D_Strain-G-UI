8c4a0f2f45c47a2e66c8fd5e0751d3e5e147b49c
jjarchtop <jj.derooy123@gmail.com>
2020-12-30 11:25:30 -0800

    Removes extra CAD

--- 3D_Backend.py
+++ 3D_Backend.py
@@ -6,20 +6,88 @@ from stl import mesh
 import sys
 import os
 from serial import Serial
+# pip3 install PyQT5 numpy numpy-stl pyserial pyqtgraph opengl
 
-
-class MainWindow(QtWidgets.QMainWindow):
-    
+class Rocket():
     # List of .STL meshes
-    mesh_models = []
+    _mesh_models = []
 
     # Angular positions
-    yaw = 0.0
-    pitch = 0.0
-    roll = 0.0
+    _yaw = 0.0
+    _pitch = 0.0
+    _roll = 0.0
+
+    _altitude = 0.0
+
+    def __init__(self, stl_dir):
+        _mesh_models = self.create_meshes(stl_dir)
+        self.setup_arduino()
     
-    # Parabola function
-    x = 0
+    def create_meshes(self, stl_dir: str) -> list[GLGridItem]:
+        # By using the -> and : str we specify the return type and argument type
+        # This makes the method harder to break because it forces the user to pass
+        # the right type.
+        
+        # Create a list of meshes from all the STL files in the STL_files folder
+        mesh_models = []
+
+        directory = stl_dir
+        for filename in os.listdir(directory):
+            if filename.endswith(".STL"):
+                print("Found .STL: " + filename)
+                stl_model = mesh.Mesh.from_file(os.path.join(directory, filename))
+                verts, I, J, K = self.stl2mesh3d(stl_model)
+                faces = np.stack((I,J,K)).T
+                new_mesh = gl.GLMeshItem(vertexes=verts, faces=faces, smooth=False)
+                mesh_models.append(new_mesh)
+
+        return mesh_models
+
+    def stl2mesh3d(self, stl_mesh):
+        # Taken from https://chart-studio.plotly.com/~empet/15434.embed
+        # It's some spooky magic that converts an stl file into a an array of points and vertices that
+        # make up the mesh model of the rocket.
+        
+        # stl_mesh is read by nympy-stl from a stl file; it is  an array of faces/triangles (i.e. three 3d points) 
+        # this function extracts the unique vertices and the lists I, J, K to define a Plotly mesh3d
+        p, q, r = stl_mesh.vectors.shape #(p, 3, 3)
+        # the array stl_mesh.vectors.reshape(p*q, r) can contain multiple copies of the same vertex;
+        # extract unique vertices from all mesh triangles
+        vertices, ixr = np.unique(stl_mesh.vectors.reshape(p*q, r), return_inverse=True, axis=0)
+        I = np.take(ixr, [3*k for k in range(p)])
+        J = np.take(ixr, [3*k+1 for k in range(p)])
+        K = np.take(ixr, [3*k+2 for k in range(p)])
+        return vertices, I, J, K
+
+    def setup_arduino(self):
+        # Stuff for reading text from arduino doing serial.println()
+        try:
+            serial_port = "/dev/ttyUSB0"
+            baud_rate = 115200 # In arduino .ino file, Serial.begin(baud_rate)
+            self.ser = Serial(serial_port, baud_rate)
+        except:
+            print("Could not set up serial connection with Arduino. Check the connection and try again!")
+
+    def update(self):
+        self.ser.flushInput()           # Get rid of old serial data
+        line = self.ser.readline()      # Read an entire line
+        line = line.strip()             # Strip \n and \r (They cause problems)
+        angles = line.split(b'\t')      # Make a list, delimiting on binary tabs
+
+        # Rotate by the difference in angle
+        for m in self._mesh_models:
+            m.rotate(self._yaw   - float(angles[2]), 1, 0, 0)
+            m.rotate(self._pitch - float(angles[1]), 0, 1, 0)
+            m.rotate(self._roll  - float(angles[0]), 0, 0, 1)
+
+        self._yaw = float(angles[2])
+        self._pitch = float(angles[1])
+        self._roll = float(angles[0])
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    # Create our rocket by passing the directory with the STLs in it
+    Olapitsky = Rocket("STL_files")
 
     alti_grid = GLGridItem()
     alti_grid_height = 0.0
@@ -30,17 +98,10 @@ class MainWindow(QtWidgets.QMainWindow):
         
         super(MainWindow, self).__init__(*args, **kwargs)
         # Load the UI Page. uic is the thing that lets us use a .ui file
-        uic.loadUi('Basic_Plot.ui', self)
+        uic.loadUi('3D_GUI.ui', self)
         
         self.setup_graph()
-        
-        # Add meshes to the graph
-        self.mesh_models = self.create_meshes()
-        for m in self.mesh_models:
-            self.graph.addItem(m)
-
         self.start_timer()
-        self.setup_arduino()
 
         # Add Altitude grid 
         self.graph.addItem(self.alti_grid)
@@ -48,9 +109,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.alti_grid.setSize(2000, 2000, 0)
         self.alti_grid.translate(0, 0, 1000)
 
+        self.add_rocket(self.Olapitsky)
+
     def start_timer(self):
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(10)
+        self.timer.setInterval(1)
         self.timer.timeout.connect(self.update_graph)
         self.timer.start()
         
@@ -84,63 +147,9 @@ class MainWindow(QtWidgets.QMainWindow):
         zgrid.setSize(2000, 2000, 0)
 
     def update_graph(self):
-        self.ser.flushInput()           # Get rid of old serial data
-        line = self.ser.readline()      # Read an entire line
-        line = line.strip()             # Strip \n and \r
-        angles = line.split(b'\t')      # Make list, delimiting on binary tabs
-
         self.altitude_grids()
-        self.altimeter_reading = self.get_altimeter()
-        self.x += 0.1
-
-        # Rotate by the difference in angle
-        for m in self.mesh_models:
-            m.rotate(self.yaw - float(angles[2]), 1, 0, 0)
-            m.rotate(self.pitch - float(angles[1]), 0, 1, 0)
-            m.rotate(self.roll - float(angles[0]), 0, 0, 1)
-
-        self.yaw = float(angles[2])
-        self.pitch = float(angles[1])
-        self.roll = float(angles[0])
-
-    def create_meshes(self):
-        # Create a list of meshes from all the STL files in the STL_files folder
-        mesh_models = []
-
-        directory = r'STL_files'
-        for filename in os.listdir(directory):
-            if filename.endswith(".STL"):
-                print("Found model: " + filename)
-                stl_model = mesh.Mesh.from_file(os.path.join(directory, filename))
-                verts, I, J, K = self.stl2mesh3d(stl_model)
-                faces = np.stack((I,J,K)).T
-                new_mesh = gl.GLMeshItem(vertexes=verts, faces=faces, smooth=False)
-                mesh_models.append(new_mesh)
-
-        return mesh_models
-      
-    def stl2mesh3d(self, stl_mesh):
-        # Taken from https://chart-studio.plotly.com/~empet/15434.embed
-        
-        # stl_mesh is read by nympy-stl from a stl file; it is  an array of faces/triangles (i.e. three 3d points) 
-        # this function extracts the unique vertices and the lists I, J, K to define a Plotly mesh3d
-        p, q, r = stl_mesh.vectors.shape #(p, 3, 3)
-        # the array stl_mesh.vectors.reshape(p*q, r) can contain multiple copies of the same vertex;
-        # extract unique vertices from all mesh triangles
-        vertices, ixr = np.unique(stl_mesh.vectors.reshape(p*q, r), return_inverse=True, axis=0)
-        I = np.take(ixr, [3*k for k in range(p)])
-        J = np.take(ixr, [3*k+1 for k in range(p)])
-        K = np.take(ixr, [3*k+2 for k in range(p)])
-        return vertices, I, J, K
-
-    def setup_arduino(self):
-        # Stuff for reading arduino serial.println()
-        try:
-            serial_port = "/dev/ttyUSB0"
-            baud_rate = 115200; # In arduino .ino file, Serial.begin(baud_rate)
-            self.ser = Serial(serial_port, baud_rate)
-        except:
-            print("Could not set up serial connection with Arduino. Check the connection and try again!")
+        self.Olapitsky.update()
+        self.Olapitsky._altitude += 0.01
 
     def altitude_grids(self):
         
@@ -162,6 +171,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def get_altimeter(self):
         return 16328 - pow(self.x - 128,2)
+
+    def add_rocket(self, R: Rocket) -> None:
+        # Add meshes to the graph
+        print(R.length())
+        for m in R._mesh_models:
+            print("Added mesh")
+            self.graph.addItem(m)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
