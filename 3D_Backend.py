6ff92f89586a7b80d23b1c1d5f8f6256459e4c72
jjarchtop <jj.derooy123@gmail.com>
2021-01-01 15:07:31 -0800

    Begins framework for strain section coloring

--- 3D_Backend.py
+++ 3D_Backend.py
@@ -9,9 +9,9 @@ from serial import Serial
 # pip3 install PyQT5 numpy numpy-stl pyserial pyqtgraph opengl
 
 class Rocket():
-    # List of .STL meshes
+    # List of meshes created from STL files
     _mesh_models = []
-
+    
     # Angular positions
     _yaw = 0.0
     _pitch = 0.0
@@ -19,29 +19,73 @@ class Rocket():
 
     _altitude = 0.0
 
+    # Set True if setup_arduino() is successful. This is to prevent a bunch of self.ser
+    # does not exist errors when update is called if there is no arduino connected.
+    _arduino_connected = False
+
+    ## Strain locations
+    # This is gonna depend on the layout of the strain gauges. For now we will assume there are r rings of n gauges mounted
+    # around the circumference of the fuselage. In this diagram:
+    # n = 6 (3 visible strain gauges represented by 's', and 3 gauges that aren't visible as they are on the other side of the fuselage)
+    # r = 5 (5 rings of strain gauges)
+    '''
+                +---------------------------------------------------------------------------------+
+                |            s              s             s             s             s           |
+      Boattail  |            s              s             s             s             s           | Nosecone
+                |            s              s             s             s             s           |
+                +---------------------------------------------------------------------------------+
+                             0              1             2             3             4             
+    '''
+    # Since the CAD of the rocket is divided up into sections, we can color each depending on the strain reading. We need a good
+    # way of accessing each section of the rocket.  
+    
+    # For this we use a dictionary. The key is of the form r-n and the value is the index in _mesh_models that corresponds to the 
+    # correct strain section
+
+    # To make reading the STL/CAD files easier, they have a standard naming system of the form whatever_you_want-strain-section_r-n
+    # Starting with the first gauge in the first ring we would name the part in CAD 'blahblahblah-strain-section_1-1' 
+    # the second 'more_crap_strain-section_1-2' etc. The first gauge in the second ring would be 'rockets_go_brrr_strain-section_2-1' etc.
+
+    # The main reason we do it this way is that not all of the entries in _mesh_models are strain sections (ie the nosecone and fins)
+    # This lets us use whatever we want for the model, but we only have to name the strain sections in solidworks.
+    # Another nice thing is that at least in the layout described above, when creating the assembly, first add a strain section,
+    # then create a circular pattern for a ring of strain sections, then a linear pattern of that ring. This *should* get you the right name
+    # for all the strain sections without too much trouble.
+
+    _strain_sections = {} 
 
     def __init__(self):
-        print("Created Rocket!\n")
-    def create_meshes(self, stl_dir: str) -> list[GLGridItem]:
+        print("Created Rocket!\n\n")
+
+    def create_meshes(self, stl_dir: str):
         # By using the -> and : str we specify the return type and argument type
         # This makes the method harder to break because it forces the user to pass
         # the right type.
         
-        # Create a list of meshes from all the STL files in the STL_files folder
-        mesh_models = []
+        # Create a list of meshes from all the STL files in the STL_files folder according
+        # to the layout specified above
 
-        directory = stl_dir
-        for filename in os.listdir(directory):
+        # See add_rocket() for more info about what enumerate does here
+        for index, filename in enumerate(os.listdir(stl_dir)):
+            
             if filename.endswith(".STL"):
                 print("Found .STL: " + filename)
-                stl_model = mesh.Mesh.from_file(os.path.join(directory, filename))
+                stl_model = mesh.Mesh.from_file(os.path.join(stl_dir, filename))
                 verts, I, J, K = self.stl2mesh3d(stl_model)
                 faces = np.stack((I,J,K)).T
                 new_mesh = gl.GLMeshItem(vertexes=verts, faces=faces, smooth=False)
-                mesh_models.append(new_mesh)
+                self._mesh_models.append(new_mesh)
+
+                # Add the strain section indices to _strain_sections
+                if filename.find("strain-section"):
+                    l = filename.split('_')                     # Split on '_'. Now l = [crap from SW naming, "strain-section", "r-n"]
+                    key = l[len(l) - 1]                         # Get the last thing in l should be ("r-n")
+                    if key not in self._strain_sections.keys(): # Shouldn't happen, but check for duplicate names
+                        self._strain_sections[key] = index
+                    else:
+                        print("Duplicate strain-section found: ", filename, " Check .STL filenames!")
 
         print("\n")
-        return mesh_models
 
     def stl2mesh3d(self, stl_mesh):
         # Taken from https://chart-studio.plotly.com/~empet/15434.embed
@@ -65,24 +109,26 @@ class Rocket():
             serial_port = "/dev/ttyUSB0"
             baud_rate = 115200 # In arduino .ino file, Serial.begin(baud_rate)
             self.ser = Serial(serial_port, baud_rate)
+            self._arduino_connected = True
         except:
-            print("Could not set up serial connection with Arduino. Check the connection and try again!")
+            print("Could not set up serial connection with Arduino. Check the connection and try again!\n\n")
 
     def update(self):
-        self.ser.flushInput()           # Get rid of old serial data
-        line = self.ser.readline()      # Read an entire line
-        line = line.strip()             # Strip \n and \r (They cause problems)
-        angles = line.split(b'\t')      # Make a list, delimiting on binary tabs
+        if self._arduino_connected:
+            self.ser.flushInput()           # Get rid of old serial data
+            line = self.ser.readline()      # Read an entire line
+            line = line.strip()             # Strip \n and \r (They cause problems)
+            angles = line.split(b'\t')      # Make a list, delimiting on binary tabs
 
-        # Rotate by the difference in angle
-        for m in self._mesh_models:
-            m.rotate(self._yaw   - float(angles[2]), 1, 0, 0)
-            m.rotate(self._pitch - float(angles[1]), 0, 1, 0)
-            m.rotate(self._roll  - float(angles[0]), 0, 0, 1)
+            # Rotate by the difference in angle
+            for m in self._mesh_models:
+                m.rotate(self._yaw   - float(angles[2]), 1, 0, 0)
+                m.rotate(self._pitch - float(angles[1]), 0, 1, 0)
+                m.rotate(self._roll  - float(angles[0]), 0, 0, 1)
 
-        self._yaw = float(angles[2])
-        self._pitch = float(angles[1])
-        self._roll = float(angles[0])
+            self._yaw = float(angles[2])
+            self._pitch = float(angles[1])
+            self._roll = float(angles[0])
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -106,16 +152,16 @@ class MainWindow(QtWidgets.QMainWindow):
         '''
         
         # Create our meshes from the directory of STL files and setup up the rest of the Rocket
-        self.Olapitsky._mesh_models = self.Olapitsky.create_meshes('STL_files')
+        self.Olapitsky.create_meshes('STL_files')
         self.add_rocket(self.Olapitsky)
         self.Olapitsky.setup_arduino()
 
         # Start the timer to update the Rocket
-        self.update_timer(60)
+        self.update_timer(1)
 
     def update_timer(self, framerate: int):
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(1000/framerate)
+        self.timer.setInterval(int(1000/framerate))
         self.timer.timeout.connect(self.update_graph)
         self.timer.start()
         
@@ -151,11 +197,14 @@ class MainWindow(QtWidgets.QMainWindow):
     def update_graph(self):
         self.Olapitsky.update()
 
-    def add_rocket(self, R: Rocket) -> None:
-        # Add meshes to the graph
-        for m in R._mesh_models:
-            print("Added mesh")
+    def add_rocket(self, R: Rocket):
+        # Add meshes to the graph. The enumerate bit is from
+        # https://stackoverflow.com/questions/3162271/get-loop-count-inside-a-python-for-loop
+        for count, m in enumerate(R._mesh_models):
+            print("Added mesh to graph. Total meshes: ", count + 1)
             self.graph.addItem(m)
+
+        print("\n")
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
