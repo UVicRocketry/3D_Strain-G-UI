bd70d76e1744c2f15d3c05c858a709087d7f115f
JJ De Rooy <jjderooy123@gmail.com>
2021-01-21 22:41:40 -0800

    Lays foundation for log file reading

--- 3D_Backend.py
+++ 3D_Backend.py
@@ -8,6 +8,7 @@ import sys
 import os
 from serial import Serial
 import math
+import linecache
 
 # pip3 install PyQT5 numpy numpy-stl pyserial pyqtgraph pyopengl
 
@@ -31,6 +32,11 @@ class Rocket():
     # Set True if setup_arduino() is successful. This is to prevent a bunch of self.ser
     # does not exist errors when update is called if there is no arduino connected
     _arduino_connected = False
+
+    # These are set by logfile_btn(). They contain the log file that we are reading from as well as the
+    # current line we are reading from. We want the line number so we can "scrub" through the data file if needed
+    _logfile_path = ""
+    _logfile_line = 0.0
 
     # How much the color of a strain section changes based on a strain reading
     # Higher numbers means more sensitive
@@ -139,56 +145,65 @@ class Rocket():
         return color
 
     def update(self):
+        # Get rid of old serial data (the baud rate isn't fast enough to keep up so the buffer fills up)
+        # self.ser.flushInput()                   
+        # This is causing some weird behaviour so leaving it out and cranking up the frame rate so the buffer doesn't overflow
+        # Should only be a problem when updating live from an arduino
+
+        # Read an entire line in the form "yaw,pitch,roll,altitude,strain1,strain2,strain3,..."
+        # We read from either live from an arduino or from a log file
         if self._arduino_connected:
-            # Get rid of old serial data (the baud rate isn't fast enough to keep up so the buffer fills up)
-            # self.ser.flushInput()                   
-            # This is causing some weird behaviour so leaving it out and cranking up the frame rate so the buffer doesn't overflow
-            # Should only be a problem when updating live from an arduino
-                                    
-            line        = self.ser.readline()       # Read an entire line in the form "yaw,pitch,roll,altitude,strain1,strain2,strain3,..."
-            line        = line.strip()              # Strip \n and \r (They cause problems)
-            list_data   = line.split(b',')          # Make a list, delimiting on binary commas
-            angles      = list_data[0:3]            # Get ypr values
-            altitude    = list_data[3:4]            # Get altitude value
-            strains     = list_data[4:]             # Get strain values. [n:] means from n to the end of the list
+            line    = self.ser.readline()  
+        else:
+            # TODO This doesn't work right now because an instance of the Rocket class cannot
+            # access the UI elements directly like this.
+            line    = linecache.getline(self._data_path, int(self.line_number_line_edit.text()))
+            self.line_number_line_edit.text()
 
-            # Rotate by the difference in angle
-            for m in self._mesh_models:
-                m.rotate(self._yaw   - float(angles[2]), 1, 0, 0)
-                m.rotate(self._pitch - float(angles[1]), 0, 1, 0)
-                m.rotate(self._roll  - float(angles[0]), 0, 0, 1)
 
-            # Update class variables to reflect new data
-            self._yaw = float(angles[2])
-            self._pitch = float(angles[1])
-            self._roll = float(angles[0])
+        line        = line.strip()              # Strip \n and \r (They cause problems)
+        list_data   = line.split(b',')          # Make a list, delimiting on binary commas
+        angles      = list_data[0:3]            # Get ypr values
+        altitude    = list_data[3:4]            # Get altitude value
+        strains     = list_data[4:]             # Get strain values. [n:] means from n to the end of the list
 
-            self._altitude = altitude
+        # Rotate by the difference in angle
+        for m in self._mesh_models:
+            m.rotate(self._yaw   - float(angles[2]), 1, 0, 0)
+            m.rotate(self._pitch - float(angles[1]), 0, 1, 0)
+            m.rotate(self._roll  - float(angles[0]), 0, 0, 1)
 
-            
-            # Update the strain section colors. self._r*self._n is the total number of strain gauges on the rocket
-            # TODO add support for strain sections with no strain gauge on them
+        # Update class variables to reflect new data
+        self._yaw = float(angles[2])
+        self._pitch = float(angles[1])
+        self._roll = float(angles[0])
 
-            for sg in range(0, self._r*self._n):
-                    
-                    # Get the index of _mesh_models that corresponds to our data point from the _strain_sections dictionary
-                    # Keys are just a plain old integer. See create_meshes() for more
+        self._altitude = altitude
 
-                    # sg is an int, so we cast to a string to access our dict value. We have to do +1 because our dict
-                    # keys are based on the solidworks names which start at 1, not 0
-                    ss_index = self._strain_sections[str(sg + 1)]
+        
+        # Update the strain section colors. self._r*self._n is the total number of strain gauges on the rocket
+        # TODO add support for strain sections with no strain gauge on them
 
-                    # Strain value from the data source. We float() because the data comes as binary, and we want decimal
-                    strain_reading = float(strains[sg])
-                    
-                    # Get a color based on the strain. 
-                    color = self.get_color(float(strain_reading))
+        for sg in range(0, self._r*self._n):
+                
+                # Get the index of _mesh_models that corresponds to our data point from the _strain_sections dictionary
+                # Keys are just a plain old integer. See create_meshes() for more
 
-                    # Update the color of the mesh
-                    
-                    # TODO works to set the mesh color initially but it doesn't change
-                    print("Set color to", color)
-                    self._mesh_models[ss_index].setColor(color)
+                # sg is an int, so we cast to a string to access our dict value. We have to do +1 because our dict
+                # keys are based on the solidworks names which start at 1, not 0
+                ss_index = self._strain_sections[str(sg + 1)]
+
+                # Strain value from the data source. We float() because the data comes as binary, and we want decimal
+                strain_reading = float(strains[sg])
+                
+                # Get a color based on the strain. 
+                color = self.get_color(float(strain_reading))
+
+                # Update the color of the mesh
+                
+                # TODO works to set the mesh color initially but it doesn't change afterwards
+                print("Set color to", color)
+                self._mesh_models[ss_index].setColor(color)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -282,7 +297,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # This makes things much easier to understand.
         self.UI_browse_btn.clicked.connect(self.browse_btn)
         self.UI_logfile_btn.clicked.connect(self.logfile_btn)
-        self.UI_clearlog_btn.clicked.connect(self.clearlog_btn)
+        self.UI_closelog_btn.clicked.connect(self.closelog_btn)
 
     def browse_btn(self):
         # This creates a file dialog box so we can select a data file
@@ -301,7 +316,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # Update the lineEdit beside "Load File" button on the GUI.
         self.UI_logfile_LE.setText(fname)
     
-    def clearlog_btn(self):
+    def closelog_btn(self):
         # Clear the text on the line edit. This is for switching to live mode
         self.UI_logfile_LE.setText("")
 
