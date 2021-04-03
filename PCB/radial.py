ad6e7e08df30eefed15feed9704debcb7b901424
JJ De Rooy <jjderooy123@gmail.com>
2021-04-02 18:38:58 -0700

    PCB

--- PCB/radial.py
+++ PCB/radial.py
@@ -29,9 +29,17 @@ print("Got board")
 # Module References or "modrefs" are the names of the modules that we would like to mess around with.
 # In our case, they are the radial wire contact pads that we want to adjust the orientation of.
 # The order of this list is also the same order that they will get laid out in radially.
-modrefs = ["TP1", "TP2", "TP3", "TP4", "TP5", "TP6", "TP7", "TP8", "TP9", 
-           "TP10", "TP11", "TP12", "TP13", "TP14", "TP15", "TP16", "TP17",
-           "TP18", "TP19", "TP20", "TP21", "TP22", "TP23", "TP24"]
+
+# For the radial wire contact pods
+# modrefs = ["TP1", "TP2", "TP3", "TP4", "TP5", "TP6", "TP7", "TP8",
+#            "TP25", "TP28",
+#            "TP9", "TP10", "TP11", "TP12", "TP13", "TP14", "TP15", "TP16",
+#            "TP26", "TP29",
+#            "TP17", "TP18", "TP19", "TP20", "TP21", "TP22", "TP23", "TP24",
+#            "TP27", "TP30"]
+
+modrefs = ["U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9", "U10", "U11", "U12"]
+
 print("Got modrefs:", modrefs)
 
 # This list will store the modules corresponding to the names in modrefs
@@ -46,7 +54,8 @@ print("Got mods:", mods)
 # Now that we have our modules we can start laying them out radially
 num_mods    = len(modrefs)      # Number of modules to lay out radially
 angle       = 360 / num_mods    # Angular increment between each module 
-radius      = 28500000          # 1.75 inches in nanometers (the default unit of kicad)
+#radius      = 28500000          # 1.122 inches in nanometers (the default unit of kicad) (for radial wire contacts)
+radius      = 22225000          # 0.875 inches (for HX711)
 offset      = -90               # We'll need to offset each angle since the default orientation
                                 # of the module is vertical
 
@@ -60,6 +69,7 @@ for i, mod in enumerate(mods):
     newpos = pcbnew.wxPoint(pos_x, pos_y)
     
     # Set the new position and angle for the module
+    print("Mod", modrefs[i]) 
     print("Setting orientation to", angle*i + offset)
     mod.SetOrientationDegrees(angle*i + offset)
 
